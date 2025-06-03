fd76a086579365b2ce3427e37220ea0a199e2ca0
mohammedboulakhrif <166185672+mohammedboulakhrif@users.noreply.github.com>
2025-06-03 15:25:41 -0400

    .

--- client2.py
+++ client2.py
@@ -1,10 +1,10 @@
 import requests
 
 #url = "http://localhost:8000/test"
-url = "https://rcw-azzure-dadrgufpefhrecbq.canadaeast-01.azurewebsites.net/"
+url = "https://rcw-azzure-dadrgufpefhrecbq.canadaeast-01.azurewebsites.net/test"
 
 
-response = requests.get(url)
+response = requests.post(url)
 try:
     response = response.json()
     print(response)
